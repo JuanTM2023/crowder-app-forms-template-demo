c10a6701f987e98dc0b74fabdabc013fab91fae0
Juan Tenorio <juan.tenorio@ticketmaster.pe>
2026-09-13 19:11:53 -0500

    test food voucher insert

--- src/modules/food-vouchers/service.ts
+++ src/modules/food-vouchers/service.ts
@@ -1,3 +1,34 @@
+import { db } from "@/lib/db";
+import { foodVouchers } from "@/lib/db/schema";
+
 export async function generateFoodVouchers(transactionId: string) {
   console.log("GENERATING FOOD VOUCHERS", transactionId);
+
+  await db.insert(foodVouchers).values({
+    voucherNumber: `TEST-${transactionId}`,
+
+    transactionId,
+
+    partnerItemUuid: "TEMP",
+
+    itemUuid: null,
+
+    customerName: "TEST",
+
+    eventName: "TEST EVENT",
+
+    show: "TEST SHOW",
+
+    sectorName: "TEST SECTOR",
+
+    sectionName: "TEST SECTION",
+
+    productName: "TEST PRODUCT",
+
+    price: 0,
+
+    serviceFee: 0,
+
+    status: "pending",
+  });
 }
