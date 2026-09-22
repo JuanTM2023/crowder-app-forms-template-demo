5ba7688b908974c1bb9b4afaaa7d99713ebdd857
Juan Tenorio <juan.tenorio@lyv.livenation.com>
2026-09-22 08:11:36 -0500

    test reporte excel 10

--- drizzle/migrations/0003_funny_silver_samurai.sql
+++ drizzle/migrations/0003_funny_silver_samurai.sql
@@ -1,11 +1,2 @@
 ALTER TABLE "food_vouchers" RENAME COLUMN "show" TO "show_name";--> statement-breakpoint
-ALTER TABLE "food_vouchers" ADD COLUMN "public_token" uuid DEFAULT gen_random_uuid() NOT NULL;--> statement-breakpoint
-ALTER TABLE "food_vouchers" ADD COLUMN "row" text;--> statement-breakpoint
-ALTER TABLE "food_vouchers" ADD COLUMN "seat" text;--> statement-breakpoint
-ALTER TABLE "food_vouchers" ADD COLUMN "customer_email" text;--> statement-breakpoint
-ALTER TABLE "food_vouchers" ADD COLUMN "qr_url" text;--> statement-breakpoint
-ALTER TABLE "food_vouchers" ADD COLUMN "table_number" text;--> statement-breakpoint
-ALTER TABLE "food_vouchers" ADD COLUMN "redeemed_by" text;--> statement-breakpoint
 ALTER TABLE "transactions" ADD COLUMN "show_id" text;--> statement-breakpoint
-ALTER TABLE "transactions" ADD COLUMN "items_snapshot" jsonb;--> statement-breakpoint
-ALTER TABLE "food_vouchers" ADD CONSTRAINT "food_vouchers_public_token_unique" UNIQUE("public_token");
